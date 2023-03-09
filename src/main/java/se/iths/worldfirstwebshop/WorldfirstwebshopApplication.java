258a3c9ad259ee44e80d0356e4cbd5cd7afe15ed
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-03-09 11:43:11 +0100

    Initial commit

---
+++ src/main/java/se/iths/worldfirstwebshop/WorldfirstwebshopApplication.java
@@ -0,0 +1,13 @@
+package se.iths.worldfirstwebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldfirstwebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldfirstwebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/iths/worldfirstwebshop/WorldfirstwebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.worldfirstwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldfirstwebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
