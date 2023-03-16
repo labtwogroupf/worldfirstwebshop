d30bd4a7727aafdb83761a80be6bf7a07b5c6ed0
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-03-16 11:51:48 +0100

    test

--- src/test/java/se/iths/worldfirstwebshop/WorldfirstwebshopApplicationTests.java
+++ src/test/java/se/iths/worldfirstwebshop/WorldfirstwebshopApplicationTests.java
@@ -2,7 +2,7 @@ package se.iths.worldfirstwebshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-/*
+
 
 @SpringBootTest
 class WorldfirstwebshopApplicationTests {
@@ -12,4 +12,4 @@ class WorldfirstwebshopApplicationTests {
     }
 
 }
-*/
+
