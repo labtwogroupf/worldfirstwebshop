3aa065bb8c8488f377defbbb5752074c60205d21
Viktormes <vikhag0722@gmail.com>
2023-03-17 09:47:02 +0100

    add fields and getters and setters to product class

---
+++ src/main/java/se/iths/worldfirstwebshop/webshop/product/Product.java
@@ -0,0 +1,34 @@
+package se.iths.worldfirstwebshop.webshop.product;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private String name;
+    private BigDecimal price;
+    private int numberInStock;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getNumberInStock() {
+        return numberInStock;
+    }
+
+    public void setNumberInStock(int numberInStock) {
+        this.numberInStock = numberInStock;
+    }
+}
