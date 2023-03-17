f4f15787f259d4c2f9a87d4b84046379c9cf1e5e
Christofer Nyström <christofer.nystrom@iths.se>
2023-03-17 11:38:23 +0100

    Adds Cart and Test

--- src/main/java/se/iths/worldfirstwebshop/webshop/storage/Stock.java
+++ src/main/java/se/iths/worldfirstwebshop/webshop/storage/Stock.java
@@ -1,20 +1,24 @@
 package se.iths.worldfirstwebshop.webshop.storage;
 
 
+import se.iths.worldfirstwebshop.webshop.product.Product;
+
 import java.math.BigDecimal;
 
 
 public interface Stock {
+    void add(Product product);
 
-    void add();
+    void remove(Product product);
 
-    void remove();
+    void remove(Long id);
 
     void print();
 
     BigDecimal totalPrice();
 
     void clear();
+
 
 
 
