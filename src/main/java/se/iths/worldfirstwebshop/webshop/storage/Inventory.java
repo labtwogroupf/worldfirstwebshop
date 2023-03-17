53b73ba84251cf999a1e98dcc942ec410bafd8af
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-03-17 10:32:50 +0100

    add method body

--- src/main/java/se/iths/worldfirstwebshop/webshop/storage/Inventory.java
+++ src/main/java/se/iths/worldfirstwebshop/webshop/storage/Inventory.java
@@ -6,34 +6,43 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Inventory implements Stock{
+public class Inventory implements Stock {
 
     private final List<Product> inventory = new ArrayList<>();
 
     @Override
     public void add(Product product) {
+        inventory.add(product);
+    }
 
-
+    @Override
+    public void remove(Product product) {
+        inventory.remove(product);
 
     }
 
     @Override
-    public void remove() {
-
+    public void remove(Long id) {
+        //add remove by id later, now removes index
+        inventory.remove(id);
     }
 
     @Override
     public void print() {
-
+        inventory.forEach(product -> System.out.println(product.getName()));
     }
 
     @Override
     public BigDecimal totalPrice() {
-        return null;
+        BigDecimal sum = null;
+        for (Product product : inventory) sum = product.getPrice();
+        return sum;
     }
+
 
     @Override
     public void clear() {
 
+        inventory.forEach(this::remove);
     }
 }
