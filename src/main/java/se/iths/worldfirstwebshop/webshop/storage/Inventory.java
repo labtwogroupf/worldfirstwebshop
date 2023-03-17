3d95cf4a8f9eeb5aa76b39b39ede0cdc198b2375
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-03-17 10:15:31 +0100

    add methods

--- src/main/java/se/iths/worldfirstwebshop/webshop/storage/Inventory.java
+++ src/main/java/se/iths/worldfirstwebshop/webshop/storage/Inventory.java
@@ -1,4 +1,39 @@
 package se.iths.worldfirstwebshop.webshop.storage;
 
-public class Inventory {
+import se.iths.worldfirstwebshop.webshop.product.Product;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory implements Stock{
+
+    private final List<Product> inventory = new ArrayList<>();
+
+    @Override
+    public void add(Product product) {
+
+
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public BigDecimal totalPrice() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
 }
