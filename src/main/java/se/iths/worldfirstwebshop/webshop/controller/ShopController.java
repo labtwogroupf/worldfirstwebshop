f1d95a0514f4b9ad2fd4c384b9e47a88fc137b10
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-03-23 13:26:45 +0100

    refactor ProductController.java to map to correct objects

--- src/main/java/se/iths/worldfirstwebshop/webshop/controller/ShopController.java
+++ src/main/java/se/iths/worldfirstwebshop/webshop/controller/ShopController.java
@@ -5,31 +5,54 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.iths.worldfirstwebshop.webshop.access.Shop;
+import se.iths.worldfirstwebshop.webshop.dto.ProductDto;
+import se.iths.worldfirstwebshop.webshop.mapper.Mapper;
 import se.iths.worldfirstwebshop.webshop.product.Product;
+import se.iths.worldfirstwebshop.webshop.repository.InventoryRepository;
+import se.iths.worldfirstwebshop.webshop.storage.InventoryEntity;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/shop")
 public class ShopController {
 
     Shop shop;
+    Mapper mapper;
+    InventoryRepository inventoryRepo;
 
-    public ShopController(Shop shop) {
+    public ShopController(Shop shop, Mapper mapper, InventoryRepository inventoryRepo) {
         this.shop = shop;
+        this.mapper = mapper;
+        this.inventoryRepo = inventoryRepo;
     }
 
     @PostMapping
-    void addToCart(@RequestBody Product product, int amount) {
-        shop.addToCart(product, amount);
+    void addToCart(@RequestBody ProductDto product, int amount) {
+        shop.addToCart(mapper.mapToProduct(product), amount);
     }
 
     @PostMapping
-    void removeFromCart(@RequestBody Product product) {
-        shop.removeFromCart(product);
+    void removeFromCart(@RequestBody ProductDto product) {
+        shop.removeFromCart(mapper.mapToProduct(product));
     }
 
     @PostMapping
     void checkout() {
         shop.checkout();
+        var currentInventory = inventoryRepo.findAll();
+        var inventoryAfterPurchase = shop.getInventory().getInventory();
+
+        for (InventoryEntity product : currentInventory)
+            checkIfExistsAndUpdateAmount(inventoryAfterPurchase,product);
+
+
+    }
+
+    private static void checkIfExistsAndUpdateAmount(Map<Product, Integer> map, InventoryEntity product) {
+        if (map.containsKey(product.getProduct()))
+            product.setAmount(map.get(product));
+
     }
 
 }
