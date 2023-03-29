5b40543240b46ea9d1e2c40f2204bf9a0f82f653
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-03-29 17:46:09 +0200

    add mapToInventory method in mapper

--- src/main/java/se/iths/worldfirstwebshop/webshop/mapper/Mapper.java
+++ src/main/java/se/iths/worldfirstwebshop/webshop/mapper/Mapper.java
@@ -1,22 +1,34 @@
 package se.iths.worldfirstwebshop.webshop.mapper;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.ApplicationScope;
 import se.iths.worldfirstwebshop.webshop.dto.InventoryDto;
 import se.iths.worldfirstwebshop.webshop.dto.ProductDto;
 import se.iths.worldfirstwebshop.webshop.product.Product;
 import se.iths.worldfirstwebshop.webshop.product.ProductEntity;
 import se.iths.worldfirstwebshop.webshop.storage.InventoryEntity;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Component
 public class Mapper {
 
-    public Mapper(){
+    public Mapper() {
 
     }
-    public InventoryDto mapToInventoryDto(InventoryEntity inv) {
 
-        return new InventoryDto(inv.getAmount(),mapToDto(inv.getProduct()));
+    public InventoryDto mapToInventoryDto(InventoryEntity inv) {
+        return new InventoryDto(inv.getAmount(), mapToDto(inv.getProduct()));
+    }
+
+    public Map<Product, Integer> mapToInventory(List<InventoryEntity> entities) {
+        Map<Product, Integer> list = new HashMap<>();
+
+        for (int i = 0; i < entities.size(); i++)
+            list.put(mapToProduct(entities.get(i).getProduct()), entities.get(i).getAmount());
+
+        return list;
     }
 
     public ProductEntity mapToEntity(Product product) {
@@ -38,11 +50,11 @@ public class Mapper {
     }
 
     public Product mapToProduct(ProductEntity product) {
-        return new Product(product.getName(), product.getPrice(), product.getIsbn(),product.getId());
+        return new Product(product.getName(), product.getPrice(), product.getIsbn(), product.getId());
     }
 
     public Product mapToProduct(ProductDto product) {
-        return new Product(product.getName(), product.getPrice(), product.getIsbn(), product.getId());
+        return new Product(product.getName(), product.getPrice(), product.getIsbn());
     }
 
     public ProductDto mapToDto(ProductEntity product) {
