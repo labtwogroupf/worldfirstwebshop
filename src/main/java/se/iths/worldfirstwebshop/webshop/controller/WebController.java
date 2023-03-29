f05bf0707bf5aa1f8c929af8a039a2d41f812507
Viktormes <vikhag0722@gmail.com>
2023-03-29 14:13:07 +0200

    Now also adds a mainPage.

--- src/main/java/se/iths/worldfirstwebshop/webshop/controller/WebController.java
+++ src/main/java/se/iths/worldfirstwebshop/webshop/controller/WebController.java
@@ -3,22 +3,20 @@ package se.iths.worldfirstwebshop.webshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import se.iths.worldfirstwebshop.webshop.dto.InventoryDto;
-import se.iths.worldfirstwebshop.webshop.mapper.Mapper;
+
 import se.iths.worldfirstwebshop.webshop.repository.InventoryRepository;
 import se.iths.worldfirstwebshop.webshop.repository.ProductRepository;
 
-import java.util.List;
+
 
 @Controller
 public class WebController {
 
-    private final Mapper mapper;
+
     private final ProductRepository productRepository;
     private final InventoryRepository inventoryRepository;
 
-    public WebController(Mapper mapper, ProductRepository repo, InventoryRepository inventoryRepository) {
-        this.mapper = mapper;
+    public WebController(ProductRepository repo, InventoryRepository inventoryRepository) {
         this.productRepository = repo;
         this.inventoryRepository = inventoryRepository;
     }
