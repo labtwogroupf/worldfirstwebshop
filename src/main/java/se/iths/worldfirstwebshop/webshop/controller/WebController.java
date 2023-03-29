4171ec98f4b28c83e7b1832fac27dc1e73bc07d5
Viktormes <vikhag0722@gmail.com>
2023-03-29 13:27:49 +0200

    Thymeleaf now prints both inventory and products.

--- src/main/java/se/iths/worldfirstwebshop/webshop/controller/WebController.java
+++ src/main/java/se/iths/worldfirstwebshop/webshop/controller/WebController.java
@@ -3,20 +3,35 @@ package se.iths.worldfirstwebshop.webshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import se.iths.worldfirstwebshop.webshop.dto.InventoryDto;
+import se.iths.worldfirstwebshop.webshop.mapper.Mapper;
+import se.iths.worldfirstwebshop.webshop.repository.InventoryRepository;
 import se.iths.worldfirstwebshop.webshop.repository.ProductRepository;
+
+import java.util.List;
 
 @Controller
 public class WebController {
 
-    private final ProductRepository repo;
+    private final Mapper mapper;
+    private final ProductRepository productRepository;
+    private final InventoryRepository inventoryRepository;
 
-    public WebController(ProductRepository repo) {
-        this.repo = repo;
+    public WebController(Mapper mapper, ProductRepository repo, InventoryRepository inventoryRepository) {
+        this.mapper = mapper;
+        this.productRepository = repo;
+        this.inventoryRepository = inventoryRepository;
     }
     @GetMapping("/showProducts")
     String products (Model model){
-        model.addAttribute("allProducts",repo.findAll());
+        model.addAttribute("allProducts", productRepository.findAll());
         return "products";
+
+    }
+    @GetMapping("/showInventory")
+    String inventories (Model model){
+        model.addAttribute("allInventories", inventoryRepository.findAll());
+        return "inventories";
 
     }
 }
