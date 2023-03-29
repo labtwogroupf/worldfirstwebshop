8b1ff724c20bc518d6bf5e713b95f777bd3aec55
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-03-29 17:43:22 +0200

    refactor Shop.java

--- src/main/java/se/iths/worldfirstwebshop/webshop/access/Shop.java
+++ src/main/java/se/iths/worldfirstwebshop/webshop/access/Shop.java
@@ -1,11 +1,10 @@
 package se.iths.worldfirstwebshop.webshop.access;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.ApplicationScope;
 import se.iths.worldfirstwebshop.webshop.product.Product;
 import se.iths.worldfirstwebshop.webshop.storage.Cart;
 import se.iths.worldfirstwebshop.webshop.storage.Inventory;
+
+import java.util.Map;
 
 public class Shop {
 
@@ -17,10 +16,14 @@ public class Shop {
         this.inventory = new Inventory();
     }
 
+    public Shop(Map<Product, Integer> inventories) {
+        this.cart = new Cart();
+        this.inventory = new Inventory(inventories);
+    }
+
     public void addToCart(Product product, int amount) {
         if (checkIfInStock(product, amount))
             cart.add(product, amount);
-
     }
 
     public void checkout() {
@@ -30,20 +33,19 @@ public class Shop {
         cart.clear();
     }
 
-    public void removeFromCart(Product product){
+    public void removeFromCart(Product product) {
         cart.remove(product, -1);
 
     }
 
     private boolean checkIfInStock(Product product, int amount) {
-
         if (!inventory.contains(product))
             return false;
 
         var inventoryStock = inventory.getNrOfProductsInStock(product);
-        var cartStock = cart.getAmountInCart(product);  //3
+        var cartStock = cart.getAmountInCart(product);
 
-        return amount <= inventoryStock - cartStock;  // 2 <= 5 - 3 == 2
+        return amount <= inventoryStock - cartStock;
     }
 
     public Cart getCart() {
@@ -57,7 +59,4 @@ public class Shop {
     private void removeBoughtItemsFromInventory(Product product) {
         inventory.remove(product, cart.getAmountInCart(product));
     }
-
-
-
 }
