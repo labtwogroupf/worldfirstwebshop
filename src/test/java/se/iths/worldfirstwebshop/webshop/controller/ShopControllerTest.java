9d06411facea8dd6baaf53a6320b73eecc935fb7
Christofer Nyström <christofer.nystrom@iths.se>
2023-03-29 14:27:47 +0200

    Tests now work as expected and verify that code methods is called

--- src/test/java/se/iths/worldfirstwebshop/webshop/controller/ShopControllerTest.java
+++ src/test/java/se/iths/worldfirstwebshop/webshop/controller/ShopControllerTest.java
@@ -1,12 +1,17 @@
 package se.iths.worldfirstwebshop.webshop.controller;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import se.iths.worldfirstwebshop.webshop.access.Shop;
+import se.iths.worldfirstwebshop.webshop.dto.ProductDto;
 import se.iths.worldfirstwebshop.webshop.mapper.Mapper;
 import se.iths.worldfirstwebshop.webshop.repository.InventoryRepository;
 import se.iths.worldfirstwebshop.webshop.service.ShopService;
@@ -14,11 +19,9 @@ import se.iths.worldfirstwebshop.webshop.storage.Cart;
 import se.iths.worldfirstwebshop.webshop.storage.Inventory;
 
 
-
-
-
-
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ShopController.class)
 class ShopControllerTest {
@@ -27,7 +30,7 @@ class ShopControllerTest {
     ShopController shopController;
     @Autowired
     MockMvc mockMvc;
-    @SpyBean
+    @MockBean
     ShopService shopService;
     @MockBean
     InventoryRepository inventoryRepo;
@@ -41,15 +44,54 @@ class ShopControllerTest {
     Shop shop;
 
 
+
     @Test
-    void addToCart() {
+    void shouldReturnStatusCreatedWhenAddingToCart() throws Exception {
+            String json = """
+             {
+             "id":1,
+             "name":"Black tea",
+             "price":59,
+             "isbn":"100"
+             }
+             """;
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/shop/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)
+                .param("amount", "1"))
+                .andExpect(status().isCreated());
+
+        verify(shopService).addToCart(any(ProductDto.class), any(Integer.class));
     }
 
     @Test
-    void removeFromCart() {
+    void shouldReturnStatusCreatedWhenRemovingFromCart() throws Exception {
+        String json = """
+             {
+             "id":1,
+             "name":"Black tea",
+             "price":59,
+             "isbn":"100"
+             }
+             """;
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/shop/remove")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                        .param("amount", "1"))
+                .andExpect(status().isCreated());
+
+        verify(shopService).removeFromCart(any(ProductDto.class));
     }
 
     @Test
-    void checkout() {
+    void shouldCheckoutAndReturnStatusOk() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/shop/checkout")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        verify(shopService, Mockito.times(1)).checkout();
     }
 }
