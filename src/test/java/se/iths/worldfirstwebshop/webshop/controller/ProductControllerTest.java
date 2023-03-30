3df3035b9da31b6dfbc1cdd719818334205a1c40
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-03-30 13:04:22 +0200

    refactor

--- src/test/java/se/iths/worldfirstwebshop/webshop/controller/ProductControllerTest.java
+++ src/test/java/se/iths/worldfirstwebshop/webshop/controller/ProductControllerTest.java
@@ -4,20 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import se.iths.worldfirstwebshop.webshop.access.Shop;
 import se.iths.worldfirstwebshop.webshop.dto.ProductDto;
 import se.iths.worldfirstwebshop.webshop.mapper.Mapper;
-import se.iths.worldfirstwebshop.webshop.repository.InventoryRepository;
-import se.iths.worldfirstwebshop.webshop.repository.ProductRepository;
 import se.iths.worldfirstwebshop.webshop.service.ProductService;
-import se.iths.worldfirstwebshop.webshop.service.ShopService;
-import se.iths.worldfirstwebshop.webshop.storage.Cart;
-import se.iths.worldfirstwebshop.webshop.storage.Inventory;
-
-import java.math.BigDecimal;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -28,24 +19,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ProductControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
-    @Autowired
-    private ProductController controller;
-    @MockBean
-    private ProductRepository repo;
+    MockMvc mockMvc;
     @MockBean
     Mapper mapper;
-
-    @MockBean
-    ShopService shopService;
-    @MockBean
-    InventoryRepository inventoryRepo;
-    @MockBean
-    Inventory inventory;
-    @SpyBean
-    Cart cart;
-    @SpyBean
-    Shop shop;
     @MockBean
     ProductService productService;
 
