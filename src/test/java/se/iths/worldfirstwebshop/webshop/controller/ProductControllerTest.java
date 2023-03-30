b039ff92e27ca4f8bd8600f0ddb4766ee33f5a7e
MikaelEdwartz <Mikael.edwartz@gmail.com>
2023-03-30 13:03:22 +0200

    add tests for productController

--- src/test/java/se/iths/worldfirstwebshop/webshop/controller/ProductControllerTest.java
+++ src/test/java/se/iths/worldfirstwebshop/webshop/controller/ProductControllerTest.java
@@ -1,31 +1,30 @@
 package se.iths.worldfirstwebshop.webshop.controller;
 
-
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import se.iths.worldfirstwebshop.webshop.access.Shop;
+import se.iths.worldfirstwebshop.webshop.dto.ProductDto;
 import se.iths.worldfirstwebshop.webshop.mapper.Mapper;
-import se.iths.worldfirstwebshop.webshop.product.ProductEntity;
+import se.iths.worldfirstwebshop.webshop.repository.InventoryRepository;
 import se.iths.worldfirstwebshop.webshop.repository.ProductRepository;
+import se.iths.worldfirstwebshop.webshop.service.ProductService;
+import se.iths.worldfirstwebshop.webshop.service.ShopService;
+import se.iths.worldfirstwebshop.webshop.storage.Cart;
+import se.iths.worldfirstwebshop.webshop.storage.Inventory;
 
 import java.math.BigDecimal;
-import java.util.List;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ProductController.class)
-
 class ProductControllerTest {
 
     @Autowired
@@ -37,65 +36,53 @@ class ProductControllerTest {
     @MockBean
     Mapper mapper;
 
-
-
-
+    @MockBean
+    ShopService shopService;
+    @MockBean
+    InventoryRepository inventoryRepo;
+    @MockBean
+    Inventory inventory;
+    @SpyBean
+    Cart cart;
+    @SpyBean
+    Shop shop;
+    @MockBean
+    ProductService productService;
 
     @Test
-    void getAllProductsShouldReturnAmount() throws Exception{
-        ProductEntity product1 = new ProductEntity();
-        product1.setName("Black tea");
-        ProductEntity product2 = new ProductEntity();
-        product2.setName("Green tea");
-
-        when(repo.findAll()).thenReturn(List.of(product1,product2));
-        var result = repo.findAll();
+    void getAllProductsShouldReturnListOfProducts() throws Exception {
         mockMvc.perform(get("/api/products"))
                 .andExpect(status().isOk());
 
-        assertEquals(2,result.size());
+        verify(productService, times(1)).getAllProducts();
+
     }
 
     @Test
-    void getAProductShouldReturnAProduct() throws Exception{
-        ProductEntity product1 = new ProductEntity();
-        product1.setName("Black tea");
-        ProductEntity product2 = new ProductEntity();
-        product2.setName("Green tea");
+    void getAProductShouldReturnAProduct() throws Exception {
 
-        when(repo.findById(2L)).thenReturn(Optional.of(product2));
-        var result = repo.findById(2L).get();
-
-        mockMvc.perform(get("/api/products/2"))
+        mockMvc.perform(get("/api/products/{id}", 1))
                 .andExpect(status().isOk());
-        assertEquals(product2,result);
-    }
-    @Test
-    void addProductShouldAddProduct() throws Exception{
-        ProductEntity product1 = new ProductEntity();
-        product1.setId(1L);
-        product1.setName("Black tea");
-        product1.setPrice(BigDecimal.valueOf(100));
-        product1.setIsbn("1234567");
 
-        when(repo.save(product1)).thenReturn(product1);
-        when(repo.findById(1L)).thenReturn(Optional.of(product1));
-        mockMvc.perform(post("/api/products")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(
-                """
+        verify(productService, times(1)).getAProduct(1L);
+    }
+
+    @Test
+    void addProductShouldAddProduct() throws Exception {
+        String json = """
                 {
                 "id":1,
-                "name":"Black Tea",
-                "price":100,
-                "isbn":"1234567"
+                "name":"Black tea",
+                "price":59,
+                "isbn":"100"
                 }
-                """))
+                """;
+        mockMvc.perform(post("/api/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
                 .andExpect(status().isCreated());
 
-
-
-
+        verify(productService, times(1)).addProduct(any(ProductDto.class));
 
     }
 }
